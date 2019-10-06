c5a34b6900b5495d3b6b34ef6a1e42bce007718b
littlebenlittle <ben.little6@gmail.com>
2019-10-06 16:34:54 -0600

    require pyyaml

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ setuptools.setup(
     name="envyaml-blittle",
     version="0",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'pyyaml>=5.1.2',
+    ],
     author="Ben Little",
     author_email="ben.little6@gmail.com",
     description="Use a simple yaml file to import environment variables.",
